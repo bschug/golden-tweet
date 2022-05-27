c4168556b5784d78a4105479bb54e1936e2761e2
ismailalizada <ismail.alizadeh@yandex.com>
2022-05-27 17:51:51 +0200

    Added index html

--- src/main/java/app/goldersocial/donationserver/DonationServerApplication.java
+++ src/main/java/app/goldersocial/donationserver/DonationServerApplication.java
@@ -2,8 +2,10 @@ package app.goldersocial.donationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class DonationServerApplication {
 
 	public static void main(String[] args) {

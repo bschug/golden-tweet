de50bc2cffce16db48a1260a0aacdbab69f0c43d
Gilad Pomer <giladpomer@gmail.com>
2022-05-27 20:54:57 +0200

    WIP backend endpoints on the client + refactoring

---
+++ golden-tweet-client/golden-tweet-client/src/utils/Backend.js
@@ -0,0 +1,37 @@
+import { axios } from 'axios'
+
+const BACKEND_URL = '';
+
+export async function userWallet() {
+    var data = await axios.get(BACKEND_URL + '/userWallet');
+    console.log(data.wallet);
+
+    /*
+ GET /userWallet
+    returns [
+        {"author":"58202349572", "wallet":"0xabd235812"},
+        {"author":"42999288562", "wallet":"0x848282fe2b"}
+    ]
+ */
+}
+
+export async function userWalletByTwitterUserId(twitterUserId) {
+    var data = await axios.get(BACKEND_URL + '/userWallet/' + twitterUserId);
+    console.log(data.wallet);
+
+    /*
+ GET /userWallet/{twitterUserId}
+    returns: {"wallet": "0xabc1234567"}
+ */
+}
+
+export async function connectWallet() {
+    var data = await axios.post(BACKEND_URL + '/connectWallet');
+    console.log(data.wallet);
+
+    /*
+ POST /connectWallet
+    data: {"wallet": "0xabc1234567"}
+     - store this wallet address as the recipient for rewards of the currently logged in twitter user
+ */
+}
